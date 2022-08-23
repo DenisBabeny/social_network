e7bd9de9defb0b77937288c13ff78bc5ba9e55f4
Denis <109683124+DenisBabeny@users.noreply.github.com>
2022-08-23 20:47:32 +0300

    update

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile =()=> {
+    return (
+        <div className={"content"}>
+            <div>
+                <img src={'https://ichef.bbci.co.uk/news/999/cpsprodpb/6D5A/production/_119449972_10.jpg'}/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My post
+                <div>New Post</div>
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post1
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
