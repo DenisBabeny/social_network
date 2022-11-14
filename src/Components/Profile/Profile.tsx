4bb61788a59dd4bb088655825e7ee28a27a86104
Swaron <swaronline0@gmail.com>
2022-11-15 02:04:27 +0300

    update NavBar interface

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {ActionTypes, PostType} from "../../Redux/store";
-
+import s from "./Profile.module.css"
 export type PostDataType = {
     profilePage: Array<PostType>
     dispatch:(action: ActionTypes)=>void
 }
 const Profile = (props: PostDataType) => {
     return (
-        <div>
+        <div className={s.content}>
             <ProfileInfo/>
             <MyPosts profilePage={props.profilePage} dispatch={props.dispatch} />
         </div>
